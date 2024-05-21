1925594a43fb31b1c03261a3d2b8de552b66314a
DrakulaGonchik <150073833+DrakulaGonchik@users.noreply.github.com>
2024-05-21 13:32:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,13 +70,13 @@ if [[ $arch = "arm64" ]]; then
         binutils="$([[ $ver = */binutils ]] && echo true || echo false)"
         
         if $binutils; then
-            additional_packages="binutils binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5"
+            additional_packages="binutils binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 libncurses5-dev"
             make_opts="CC=clang"
             host_make_opts="HOSTCC=clang HOSTCXX=clang++"
         else
             # Most android kernels still need binutils as the assembler, but it will
             # not be used when the Makefile is patched to make use of LLVM_IAS option
-            additional_packages="binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi"
+            additional_packages="binutils-aarch64-linux-gnu binutils-arm-linux-gnueabi libtinfo5 libncurses5-dev"
             make_opts="CC=clang LD=ld.lld NM=llvm-nm AR=llvm-ar STRIP=llvm-strip OBJCOPY=llvm-objcopy"
             make_opts+=" OBJDUMP=llvm-objdump READELF=llvm-readelf LLVM_IAS=1"
             host_make_opts="HOSTCC=clang HOSTCXX=clang++ HOSTLD=ld.lld HOSTAR=llvm-ar"
